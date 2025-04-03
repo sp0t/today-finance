4b74e30dec34d677dd8d670dfb76556103343799
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-03 05:36:00 -0700

    [dev]

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -247,7 +247,7 @@ const OnboardingScreen = () => {
               <PrimaryButton
                 title="Let's go!"
                 style={{ marginTop: '40%' }}
-                onPress={() => router.replace('/(tabs)/explore')}
+                onPress={() => router.navigate({pathname: '/(tabs)/explore'})}
               />
             </View>
           </View>
