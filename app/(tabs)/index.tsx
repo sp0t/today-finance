d868911fe45f7062f31c4ffb849647b1cdffa98c
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-04 04:39:31 -0700

    [dev]

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -574,7 +574,7 @@ const styles = StyleSheet.create<IStyles>({
   cardImage: {
     ...StyleSheet.absoluteFillObject,
     width: '100%',
-    height: '100%',
+    height: 146,
     opacity: 0.7,
   },
   cardContent: {
