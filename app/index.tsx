1537b4eb974ada5c0ae5d9f1d6e1c147de31ce4a
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-07 08:08:56 -0700

    [dev]

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import { Redirect } from "expo-router";
+import { useRouter } from 'expo-router';
+import { usePrivy } from '@privy-io/expo';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 
 const Index = () => {
-	return <Redirect href="/(tabs)" />;
+  const router = useRouter();
+  const { user, isReady } = usePrivy();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (isReady) {
+      console.log("user:", user);
+      if (!user) {
+      } else {
+        console.log("Authenticated, staying on tabs");
+      }
+      setIsLoading(false);
+    }
+  }, [isReady, router]);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return <Redirect href="/(tabs)" />;
 };
 export default Index;
