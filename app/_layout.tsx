e1a0d860150b88185c6c6164d2f74c8cabe09ea7
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-07 01:45:09 -0700

    [dev]

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Toaster } from "react-native-customizable-toast";
 import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from '@expo-google-fonts/inter';
 import { useFonts } from 'expo-font';
+import Constants from "expo-constants";
 
 
 export default function RootLayout() {
@@ -21,8 +22,8 @@ export default function RootLayout() {
 
   return (
     <PrivyProvider
-      appId="cm76p206400ey5b2sgwsn2acf"
-      clientId="client-WY5gxmNC7Y288p2SAnCrTgb5kKnfAhB63h4RgUyZNaNM3"
+      appId={Constants.expoConfig?.extra?.privyAppId}
+      clientId={Constants.expoConfig?.extra?.privyClientId}
       config={{
         embedded: {
           ethereum: {
@@ -32,16 +33,16 @@ export default function RootLayout() {
       }}
     >
       {/* <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}> */}
-        <GestureHandlerRootView>
-          <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-          <Toaster />
-        </GestureHandlerRootView>
+      <GestureHandlerRootView>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+        <Toaster />
+      </GestureHandlerRootView>
       {/* </ThemeProvider> */}
       <PrivyElements></PrivyElements>
     </PrivyProvider>
