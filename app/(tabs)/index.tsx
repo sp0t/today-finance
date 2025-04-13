9ab6bc102cff9b4939925d2340b19f660d338850
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-13 06:30:58 -0700

    [dev]-top gainers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -712,7 +712,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    height: height - 20,
+    height: height - 40,
     paddingTop: 20,
   },
   modalHeader: {
