ea3437c8bd653992607f1b0d007727da7d1f7db7
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-13 22:17:10 -0700

    [dev]-top gainers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1125,7 +1125,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 40,
+    marginTop: 70,
     marginBottom: 16,
   },
   currencySymbol: {
