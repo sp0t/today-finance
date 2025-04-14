0a5c381a8a3d2021fb919b764a328214df651f9a
sp0t <ryan.paliukaitis.my@gmail.com>
2025-04-14 06:11:26 -0700

    [dev]-top gainers

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -538,12 +538,12 @@ const TokenDetailModal: React.FC<TokenDetailModalProps> = ({
                     <View style={[styles.depositButtonContainer, {flexDirection: 'row', gap:20}]}>
                       <PrimaryButton
                         title="Buy"
-                        style={{ width: "45%" }}
+                        style={{ width: width * 0.5 -10 }}
                         onPress={handleDeposit}
                       />
                       <PrimaryButton
                         title="Sell"
-                        style={{ width: "45%" }}
+                        style={{ width: width * 0.5 -10 }}
                         onPress={handleDeposit}
                       />
                     </View>}
